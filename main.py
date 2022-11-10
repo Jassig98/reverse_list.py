9e67b9cdbe7d98ea44a921ef8afe1b070646aeb5
jasmi_ugm7gn6 <singjasm@oregonstate.edu>
2022-11-09 20:25:30 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Author: Jasmine Singh
+# Github Username: Jassig98
+# Date: November 8, 2022
+# Description: takes a list and reverses its order
+
+def reverse_list(list1):
+"""reverses order of listed values"""
+    i=0
+    j= len(list1)-1
+    while(i<j):
+        t=list1[i]
+        list1[i]=list1[j]
+        list1[j]=tmp
+        i+=1
+        j-=1
